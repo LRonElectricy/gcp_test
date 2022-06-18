3e1725044d2ae12d8a7e0571ffe2bd2441a37ff7
LRonElectricy <LRon006900@gmail.com>
2022-06-19 00:42:09 +0300

    1

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-//-----------------------------------------------------------------------------
-// NodeJS App for GCP Cloud Functions deployed via GCP Cloud Build Triggers
-// Copyleft (c) by Denis Astahov
-//-----------------------------------------------------------------------------
+const express = require('express')
+const app = express()
 
-exports.helloWorld = (req, res) => {
-    const message="<font color='blue'>СloudFunction of Denis Astahov!</font><br><b>App Version 1.1 LRon</b>";
-    res.status(200).send(message);
-  };
+// app.get('/',(req, res)=>res.send('LRON'))
+app.use(express.static('dist'))
+
+app.listen(8080,()=>console.log('running'))

a1a603d5e8aa48d7e4189aa1f0e294100156b617
LRonElectricy <LRon006900@gmail.com>
2022-06-19 00:46:53 +0300

    2

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
 
-// app.get('/',(req, res)=>res.send('LRON'))
-app.use(express.static('dist'))
+// // app.get('/',(req, res)=>res.send('LRON'))
+// app.use(express.static('dist'))
 
-app.listen(8080,()=>console.log('running'))
+// app.listen(8080,()=>console.log('running'))
+
+//-----------------------------------------------------------------------------
+// NodeJS App for GCP Cloud Functions deployed via GCP Cloud Build Triggers
+// Copyleft (c) by Denis Astahov
+//-----------------------------------------------------------------------------
+
+exports.helloWorld = () => {
+    // app.get('/',(req, res)=>res.send('LRON'))
+    app.use(express.static('dist'))
+
+    app.listen(8080, () => console.log('running'))
+};

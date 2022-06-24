042c4374f21e1a1900c42f4145fa1c264d551ca6
LRonElectricy <LRon006900@gmail.com>
2022-06-24 07:29:42 +0300

    1

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ const express = require('express')
 
 
 exports.helloWorld = (req, res) => {
-  const app = express()
+  // const app = express()
 
   // app.get('/',(req, res)=>res.send('LRON'))
-  app.use(express.static('dist'))
+  // app.use(express.static('dist'))
 
-  app.listen(80, () => console.log('running'))
-  // const message = "<font color='blue'>СloudFunction of Denis Astahov!</font><br><b>App Version 1.2 staging!</b>";
-  // res.status(200).send(message);
+  // app.listen(80, () => console.log('running'))
+  const message = "<font color='blue'>СloudFunction of Denis Astahov!</font><br><b>App Version 1.2 staging!</b>";
+  res.status(200).send(message);
 };
